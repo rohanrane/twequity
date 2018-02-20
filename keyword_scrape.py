e2f93186f1dbbeac7e20aea8e05b72c118bc848a
Erik Agren <eagren20@gmail.com>
2018-02-20 11:49:22 -0500

    changed output filename and added checker for correct cmd line args

--- keyword_scrape.py
+++ keyword_scrape.py
@@ -18,6 +18,11 @@ if len(sys.argv) != 3:
 	print "run using the following command line arguments: python keyword_scrape.py CSVFILE.csv KEYWORDFILE.txt"
 	sys.exit(0)
 
+if (not('.csv' in sys.argv[1]) or not('.txt' in sys.argv[2])):
+	print "run using the following command line arguments: python keyword_scrape.py CSVFILE.csv KEYWORDFILE.txt"
+	sys.exit(0)
+
+
 with open(sys.argv[1]) as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV:
@@ -39,7 +44,7 @@ for date, handle, ID in itertools.izip(dates, handles, eventIDs):
 
 	print handle + ":"
 	print "Event Date: ", event_date
-	print "Start Date:: ", start_date
+	print "Start Date: ", start_date
 	print "End Date: ", end_date
 
 	tweetCriteria = got.manager.TweetCriteria().setUsername(handle).setSince(start_date).setUntil(end_date)
@@ -49,11 +54,18 @@ for date, handle, ID in itertools.izip(dates, handles, eventIDs):
 		keywordCriteria = tweetCriteria.setQuerySearch(keyword)
 		tweets = tweets + got.manager.TweetManager.getTweets(keywordCriteria)
 
-	print "Total Tweets: ", len(tweets)
-	filename = str(ID) + "_" + handle + ".csv"
+	
+	print "Total Tweets(before dup removal): ", len(tweets)
+	noDupTweets = tweets
+	# Hopefully this isn't too expensive
+	# temp_set = set(map(tuple,tweets))
+	# noDupTweets = map(list,temp_set)
+
+	print "Total Tweets: ", len(noDupTweets)
+	filename = str(ID) + "_" + handle + "_keywords" + ".csv"
 
 	with open(filename, "w") as output:
 	        writer = csv.writer(output, delimiter=',')
-	        for t in tweets:
+	        for t in noDupTweets:
 	        	row = t.date, t.text, t.retweets, t.favorites, t.mentions, t.hashtags
 	        	writer.writerow([unicode(s).encode("utf-8") for s in row])
