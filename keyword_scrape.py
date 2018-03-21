2c78c3bde0cb9a1e5ea21630ae64e2dfd815c930
Erik Agren <eagren20@gmail.com>
2018-03-20 22:41:17 -0400

    Changed to scrape all of twitter, may need more testing

--- keyword_scrape.py
+++ keyword_scrape.py
@@ -5,7 +5,11 @@ import itertools
 from datetime import datetime, timedelta
 from dateutil import parser
 
+#TODO: needs to scrape all of twitter for tweets with a keyword AND one of either the company twitter handle or company name
+#TODO: also write the user who tweeted to row in csv file
+
 dates = []
+names = []
 handles = []
 eventIDs = []
 keywords = []
@@ -29,6 +33,7 @@ with open(sys.argv[1]) as csvfile:
     	date = datetime.strptime(row[3], "%m/%d/%Y").strftime("%Y-%m-%d")
         dates.append(date)
         handles.append(row[4])
+        names.append(row[2])
         eventIDs.append(row[0])
 
 with open(sys.argv[2]) as keywordFile:
@@ -36,7 +41,7 @@ with open(sys.argv[2]) as keywordFile:
 	for line in lines:
 		keywords.append(line)
 
-for date, handle, ID in itertools.izip(dates, handles, eventIDs):
+for date, handle, ID, name in itertools.izip(dates, handles, eventIDs, names):
 	event_date = parser.parse(date)
 
 	start_date = (event_date - timedelta(days=days_before)).strftime("%Y-%m-%d")
@@ -47,33 +52,50 @@ for date, handle, ID in itertools.izip(dates, handles, eventIDs):
 	print "Start Date: ", start_date
 	print "End Date: ", end_date
 
-	tweetCriteria = got.manager.TweetCriteria().setUsername(handle).setSince(start_date).setUntil(end_date)
+	tweetCriteria = got.manager.TweetCriteria().setSince(start_date).setUntil(end_date)
 
-	#reset tweet list for each company
-	tweets = []
 
-	#loop through keywords and scrape the tweets
+	#build tweet query
+	query = ''
+	#add company name queries
 	for keyword in keywords:
-		keywordCriteria = tweetCriteria.setQuerySearch(keyword)
-		tweets = tweets + got.manager.TweetManager.getTweets(keywordCriteria)
+		query = query + name + ' AND ' + keyword +' OR '
+	#add company handle queries
+	for keyword in keywords:
+		query = query + handle + ' AND ' + keyword +' OR '
 
-	
-	print "Total Tweets(with duplicates): ", len(tweets)
+	#get rid of OR at end
+	query = query[:-3]
+	#turn it into a list
+	queries = query.split(' OR ')
 
-	# Get rid of duplicate tweets
-	# Hopefully this isn't too expensive
+	# for q in queries:
+	# 	print q
+
+	#First scrape for company name
+	# keywordCriteria = tweetCriteria.setQuerySearch()
+	# tweets = got.manager.TweetManager.getTweets(keywordCriteria)
+	# print "added name tweets"
+
+	#loop through queries and collect tweets for each
 	ids = set()
 	noDupTweets = []
-	for tweet in tweets:
-		if not tweet.id in ids:
-			ids.add(tweet.id)
-			noDupTweets.append(tweet)
+	for q in queries:
+		print 'Query: '+ q
+		keywordCriteria = tweetCriteria.setQuerySearch(q)
+		tweets = got.manager.TweetManager.getTweets(keywordCriteria)
+		print 'Total tweets this query: ' + str(len(tweets))
+		#remove duplicates
+		for tweet in tweets:
+			if not tweet.id in ids:
+				ids.add(tweet.id)
+				noDupTweets.append(tweet)
 
-	print "Total Tweets(without duplicates): ", len(noDupTweets)
+	print "Total Tweets: ", len(noDuptweets)
 	filename = str(ID) + "_" + handle + "_keywords" + ".csv"
 
 	with open(filename, "w") as output:
-	        writer = csv.writer(output, delimiter=',')
-	        for t in noDupTweets:
-	        	row = t.date, t.text, t.retweets, t.favorites, t.mentions, t.hashtags, t.id
-	        	writer.writerow([unicode(s).encode("utf-8") for s in row])
+	    writer = csv.writer(output, delimiter=',')
+	    for t in noDupTweets:
+	        row = t.date, t.text, t.retweets, t.favorites, t.mentions, t.hashtags, t.id
+	        writer.writerow([unicode(s).encode("utf-8") for s in row])
