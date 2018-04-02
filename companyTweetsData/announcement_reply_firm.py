d05a36360b7635a1e5ba343bfe7216ca18790f9c
cwiskur <kingwisk@vt.edu>
2018-04-02 19:24:18 -0400

    announcment fix

--- companyTweetsData/announcement_reply_firm.py
+++ companyTweetsData/announcement_reply_firm.py
@@ -1,7 +1,6 @@
 import numpy
 from numpy import nan
 import pandas 
-import matplotlib.pyplot
 import glob
 path = "*.csv"
 
@@ -9,3 +8,4 @@ for fname in glob.glob(path):
     table = pandas.read_csv(fname, header=None)
     table[len(table.columns)] = ["Announcement" if x is nan else "Reply" for x in table[4]]
     table.to_csv(fname)
+    print('Appended announcement column to', fname)
