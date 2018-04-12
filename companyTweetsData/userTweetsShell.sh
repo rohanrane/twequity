214c2c38a43b105c2f60b32857219aab0eb9c5c1
cwiskur <kingwisk@vt.edu>
2018-04-12 11:56:06 -0400

    fixed script and modidifed shell to not run some things

--- companyTweetsData/userTweetsShell.sh
+++ companyTweetsData/userTweetsShell.sh
@@ -1,5 +1,5 @@
 #run with ./userTweetsShell.sh
 python countURLs.py
 python announcement_reply_firm.py
-python user_sentiment.py
-python profile_scrape.py
+# python user_sentiment.py
+# python profile_scrape.py
--- companyTweetsData/user_sentiment.py
+++ companyTweetsData/user_sentiment.py
@@ -8,7 +8,7 @@ from textblob import TextBlob
 from textblob.sentiments import NaiveBayesAnalyzer
 
 def clean_tweet(tweet):
-        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
 for fname in glob.glob('*.csv'):
     table = pd.read_csv(fname)
@@ -18,9 +18,12 @@ for fname in glob.glob('*.csv'):
     table[len(table.columns)] = ""
     for index in table.iterrows():
         string = table.ix[count,1]
-        analysis = TextBlob(clean_tweet(string), analyzer=NaiveBayesAnalyzer())
-        table.ix[count,len(table.columns)-3] = analysis.sentiment.classification
-        table.ix[count,len(table.columns)-2] = analysis.sentiment.p_pos
-        table.ix[count,len(table.columns)-1] = analysis.sentiment.p_neg
+        if(string != None)
+        {
+            analysis = TextBlob(clean_tweet(string), analyzer=NaiveBayesAnalyzer())
+            table.ix[count,len(table.columns)-3] = analysis.sentiment.classification
+            table.ix[count,len(table.columns)-2] = analysis.sentiment.p_pos
+            table.ix[count,len(table.columns)-1] = analysis.sentiment.p_neg
+        }
         count = count + 1
 table.to_csv(fname, index=False, header=False)
